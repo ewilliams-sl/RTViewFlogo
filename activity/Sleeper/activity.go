ed37ce86454013cdfed09a13a37932c2638e180b
ewilliams-sl <everett.williams@sl.com>
2019-06-07 14:58:45 -0700

    Code Cleanup

--- activity/Sleeper/activity.go
+++ activity/Sleeper/activity.go
@@ -32,18 +32,14 @@ func sleepFor(iLow int, iRand int) int{
 		return iSleep
 }
 
-
-// NewActivity creates a new activity
 func NewActivity(metadata *activity.Metadata) activity.Activity {
 	return &MyActivity{metadata: metadata}
 }
 
-// Metadata implements activity.Activity.Metadata
 func (a *MyActivity) Metadata() *activity.Metadata {
 	return a.metadata
 }
 
-// Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	fMin := context.GetInput(spMinMillis).(float64)
@@ -54,7 +50,6 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	
 	sleepFor(iMin, iRnd)
 
-	// Signal to the Flogo engine that the activity is completed
 	return true, nil
 	
 }
